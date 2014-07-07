0996e37612c1cc9ffa7a0ae5544f5560001f7a1b
volgyiadam <volgyiadam@gmail.com>
2014-07-07 18:05:49 +0200

    car async ajax post call

--- gepjarmuKereskedes/Controllers/CarController.cs
+++ gepjarmuKereskedes/Controllers/CarController.cs
@@ -194,10 +194,28 @@ namespace gepjarmuKereskedes.Controllers
         ////
         //// POST: /Auto/Delete/5
 
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        //[Authorize]
-        public ActionResult DeleteConfirmed(int id)
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        ////[Authorize]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    using (ISession session = NHibernateSession.OpenSession())
+        //    {
+        //        using (ITransaction trans = session.BeginTransaction())
+        //        {
+        //            Car auto = session.Get<Car>(id);
+        //            session.Get<Site>(auto.SiteId).ReservedCount -= 1;
+
+        //            session.Delete(auto);
+        //            trans.Commit();
+        //            return RedirectToAction("Index");
+        //        }
+
+        //    }
+        //}
+
+        [HttpPost]
+        public JsonResult DeleteConfirmed(int id)
         {
             using (ISession session = NHibernateSession.OpenSession())
             {
@@ -208,12 +226,10 @@ namespace gepjarmuKereskedes.Controllers
 
                     session.Delete(auto);
                     trans.Commit();
-                    return RedirectToAction("Index");
+                    return new JsonResult() { Data = new { success = true }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
                 }
 
             }
         }
-
-
     }
 }
